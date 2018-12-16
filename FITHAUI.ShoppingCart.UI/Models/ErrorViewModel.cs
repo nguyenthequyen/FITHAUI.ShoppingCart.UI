5b9494f7dd9b7726ed00564788115deda0d7c1b6
Nguyễn Thế Quyền <thequyen96haui@outlook.com.vn>
2018-12-16 16:28:17 +0700

    Add project files.

---
+++ FITHAUI.ShoppingCart.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FITHAUI.ShoppingCart.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
