e838ce3744ecfc021a9cfe7755609d3073325d1e
Nguyễn Thế Quyền <thequyen96haui@outlook.com.vn>
2018-12-20 13:33:29 +0700

    config sessionn

--- FITHAUI.ShoppingCart.UI/Startup.cs
+++ FITHAUI.ShoppingCart.UI/Startup.cs
@@ -49,7 +49,7 @@ namespace FITHAUI.ShoppingCart.UI
             services.AddSession(options =>
             {
                 // Set a short timeout for easy testing.
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
                 options.Cookie.HttpOnly = true;
             });
 
